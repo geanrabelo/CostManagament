b16bc56e0b7c7f68b501db4ddf5f19e8fb2b6309
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-22 13:49:14 -0300

    update CostCreateDTO - added @JsonFormat

--- src/main/java/com/br/CostManagement/dto/request/cost/CostCreateDTO.java
+++ src/main/java/com/br/CostManagement/dto/request/cost/CostCreateDTO.java
@@ -1,10 +1,13 @@
 package com.br.CostManagement.dto.request.cost;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record CostCreateDTO(String description,
                             BigDecimal value,
+                            @JsonFormat(pattern = "dd/MM/yyyy")
                             LocalDate date,
                             Long category_id,
                             Long user_id) {
