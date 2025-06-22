77b14b295b6acc154c40c3f0c8bbadb6978f09e5
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-22 16:02:42 -0300

    update CostRepository - added method findByCostWhereCategoryId

--- src/main/java/com/br/CostManagement/repository/CostRepository.java
+++ src/main/java/com/br/CostManagement/repository/CostRepository.java
@@ -2,8 +2,14 @@ package com.br.CostManagement.repository;
 
 import com.br.CostManagement.entity.Cost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CostRepository extends JpaRepository<Cost, Long> {
 
     boolean existsByDescription(String description);
+
+    @Query("select c from Cost c WHERE c.category.id = ?1")
+    List<Cost> findByCostWhereCategoryId(Long id);
 }
