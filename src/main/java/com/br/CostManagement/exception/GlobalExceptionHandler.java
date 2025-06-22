4d6a61accfcbdf5e2ffa2395060ca815a18a54be
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-22 11:50:06 -0300

    create GlobalExceptionHandler - added UserNotFound

--- src/main/java/com/br/CostManagement/exception/GlobalExceptionHandler.java
+++ src/main/java/com/br/CostManagement/exception/GlobalExceptionHandler.java
@@ -1,4 +1,18 @@
 package com.br.CostManagement.exception;
 
+import com.br.CostManagement.exception.dto.ErrorResponse;
+import com.br.CostManagement.exception.ex.UserNotFound;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFound.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse userNotFoundHandler(UserNotFound userNotFound){
+        return ErrorResponse.notFound(userNotFound.getMessage());
+    }
 }
