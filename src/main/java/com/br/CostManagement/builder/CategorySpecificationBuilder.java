450fe377a7f415dfeb802cae4e8a657e30d22173
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-22 10:36:34 -0300

    create CategorySpecificationBuilder

---
+++ src/main/java/com/br/CostManagement/builder/CategorySpecificationBuilder.java
@@ -0,0 +1,12 @@
+package com.br.CostManagement.builder;
+import com.br.CostManagement.entity.Category.CategoryBuilder;
+import com.br.CostManagement.entity.User;
+
+public interface CategorySpecificationBuilder {
+
+    CategoryBuilder id(Long id);
+
+    CategoryBuilder name(String name);
+
+    CategoryBuilder user(User user);
+}
