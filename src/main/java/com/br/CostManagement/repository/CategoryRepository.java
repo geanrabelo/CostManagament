3ae6ede66e7a8490dacb3c38732dd529f75ed5a5
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-22 16:02:10 -0300

    update CategoryRepository - added method findByCategoryWhereUserId

--- src/main/java/com/br/CostManagement/repository/CategoryRepository.java
+++ src/main/java/com/br/CostManagement/repository/CategoryRepository.java
@@ -2,8 +2,14 @@ package com.br.CostManagement.repository;
 
 import com.br.CostManagement.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     boolean existsByName(String name);
+
+    @Query("select c from Category c WHERE c.user.id = ?1")
+    List<Category> findByCategoryWhereUserId(Long userId);
 }
