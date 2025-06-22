2d3426491efa614912266f6a13666912c9cd9e87
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-22 10:41:01 -0300

    create CategoryBuilder implements CategorySpecificationBuilder

--- src/main/java/com/br/CostManagement/entity/Category.java
+++ src/main/java/com/br/CostManagement/entity/Category.java
@@ -1,5 +1,6 @@
 package com.br.CostManagement.entity;
 
+import com.br.CostManagement.builder.CategorySpecificationBuilder;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,4 +22,36 @@ public class Category {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    public static final class CategoryBuilder implements CategorySpecificationBuilder {
+        private Long id;
+        private String name;
+        private User user;
+
+        public CategoryBuilder builder(){
+            return new CategoryBuilder();
+        }
+
+        @Override
+        public CategoryBuilder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        @Override
+        public CategoryBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        @Override
+        public CategoryBuilder user(User user) {
+            this.user = user;
+            return this;
+        }
+
+        public Category build(){
+            return new Category(this.id, this.name, this.user);
+        }
+    }
 }
