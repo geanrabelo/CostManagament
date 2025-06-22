62861fb9367bc897eab5bc77fb44598e15cc5241
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-22 11:46:11 -0300

    create ErrorResponse

--- src/main/java/com/br/CostManagement/exception/dto/ErrorResponse.java
+++ src/main/java/com/br/CostManagement/exception/dto/ErrorResponse.java
@@ -1,4 +1,17 @@
 package com.br.CostManagement.exception.dto;
 
-public record ErrorResponse() {
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+public record ErrorResponse(int status,
+                            String message,
+                            List<ErrorField> errorFieldList) {
+    public static ErrorResponse notFound(String message){
+        return new ErrorResponse(HttpStatus.NOT_FOUND.value(), message, List.of());
+    }
+
+    public static ErrorResponse conflict(String message){
+        return new ErrorResponse(HttpStatus.CONFLICT.value(), message, List.of());
+    }
 }
