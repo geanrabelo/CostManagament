e5477377acc6ec0c2fad08dcf45f61e2ce19e751
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-22 13:14:59 -0300

    create CostService

--- src/main/java/com/br/CostManagement/service/CostService.java
+++ src/main/java/com/br/CostManagement/service/CostService.java
@@ -1,4 +1,20 @@
 package com.br.CostManagement.service;
 
+import com.br.CostManagement.dto.request.cost.CostCreateDTO;
+import com.br.CostManagement.dto.request.cost.CostUpdateDTO;
+import com.br.CostManagement.dto.response.cost.CostDetailsDTO;
+
+import java.util.List;
+
 public interface CostService {
+
+    String create(CostCreateDTO costCreateDTO);
+
+    CostDetailsDTO update(CostUpdateDTO costUpdateDTO);
+
+    CostDetailsDTO findById(Long id);
+
+    List<CostDetailsDTO> findAll();
+
+    void delete(Long id);
 }
