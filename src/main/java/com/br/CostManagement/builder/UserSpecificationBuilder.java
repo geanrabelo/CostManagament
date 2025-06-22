4077c965b38e8786643aa18166cb690e0efbc243
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-22 10:28:50 -0300

    update UserSpecificationBuilder - fix returns

--- src/main/java/com/br/CostManagement/builder/UserSpecificationBuilder.java
+++ src/main/java/com/br/CostManagement/builder/UserSpecificationBuilder.java
@@ -1,14 +1,14 @@
 package com.br.CostManagement.builder;
 
-import com.br.CostManagement.entity.User;
+import com.br.CostManagement.entity.User.UserBuilder;
 
 import java.math.BigDecimal;
 
 public interface UserSpecificationBuilder {
 
-    User.UserBuilder id(Long id);
+    UserBuilder id(Long id);
 
-    User.UserBuilder name(String name);
+    UserBuilder name(String name);
 
-    User.UserBuilder salary(BigDecimal salary);
+    UserBuilder salary(BigDecimal salary);
 }
