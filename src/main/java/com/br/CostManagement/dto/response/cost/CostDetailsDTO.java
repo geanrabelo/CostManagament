a2894d37aa0d9114cf5eab56b4e91e7813dd777a
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-22 11:30:34 -0300

    create CostDetailsDTO

--- src/main/java/com/br/CostManagement/dto/response/cost/CostDetailsDTO.java
+++ src/main/java/com/br/CostManagement/dto/response/cost/CostDetailsDTO.java
@@ -1,4 +1,17 @@
 package com.br.CostManagement.dto.response.cost;
 
-public record CostDetailsDTO() {
+import com.br.CostManagement.entity.Cost;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record CostDetailsDTO(Long id,
+                             String description,
+                             BigDecimal value,
+                             LocalDate date,
+                             Long category_id,
+                             Long user_id) {
+    public CostDetailsDTO(Cost cost){
+        this(cost.getId(), cost.getDescription(), cost.getValue(), cost.getDate(), cost.getCategory().getId(), cost.getUser().getId());
+    }
 }
