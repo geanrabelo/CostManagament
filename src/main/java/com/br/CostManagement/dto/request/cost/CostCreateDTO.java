fbdc749cbc18d238a05d49cf4d7b6f048ab1ec23
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-22 11:24:29 -0300

    create CostCreateDTO

--- src/main/java/com/br/CostManagement/dto/request/cost/CostCreateDTO.java
+++ src/main/java/com/br/CostManagement/dto/request/cost/CostCreateDTO.java
@@ -1,4 +1,12 @@
 package com.br.CostManagement.dto.request.cost;
 
-public record CostCreateDTO() {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record CostCreateDTO(String description,
+                            BigDecimal value,
+                            LocalDate date,
+                            Long category_id,
+                            Long user_id) {
+
 }
