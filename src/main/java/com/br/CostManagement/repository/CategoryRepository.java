3e09e5a3d76994cd4b08d442ff028df5fdf655a2
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-22 10:56:47 -0300

    create CategoryRepository

---
+++ src/main/java/com/br/CostManagement/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.br.CostManagement.repository;
+
+import com.br.CostManagement.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
