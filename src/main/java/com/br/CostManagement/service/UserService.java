62d9cea85dd273e9c41862b0b62a770db70534f1
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-22 16:03:15 -0300

    update UserService - added method calculateCost

--- src/main/java/com/br/CostManagement/service/UserService.java
+++ src/main/java/com/br/CostManagement/service/UserService.java
@@ -2,6 +2,7 @@ package com.br.CostManagement.service;
 
 import com.br.CostManagement.dto.request.user.UserAddSalaryDTO;
 import com.br.CostManagement.dto.request.user.UserCreateDTO;
+import com.br.CostManagement.dto.response.user.UserCalculateCostDTO;
 import com.br.CostManagement.dto.response.user.UserDetailsDTO;
 
 import java.util.List;
@@ -9,6 +10,8 @@ import java.util.List;
 public interface UserService {
 
     String create(UserCreateDTO userCreateDTO);
+
+    UserCalculateCostDTO calculateCost(Long id);
 
     UserDetailsDTO addSalary(UserAddSalaryDTO userAddSalaryDTO);
 
