a0f6e9e17d12375eb68e866796fbe6d96e83b027
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-22 13:24:28 -0300

    update CostRepository - added method -> existsByDescription

--- src/main/java/com/br/CostManagement/repository/CostRepository.java
+++ src/main/java/com/br/CostManagement/repository/CostRepository.java
@@ -4,4 +4,6 @@ import com.br.CostManagement.entity.Cost;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CostRepository extends JpaRepository<Cost, Long> {
+
+    boolean existsByDescription(String description);
 }
