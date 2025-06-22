a4318d31d2ee3a5a41c2f655e941bc059863142f
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-22 12:06:13 -0300

    create UserController

--- src/main/java/com/br/CostManagement/controller/UserController.java
+++ src/main/java/com/br/CostManagement/controller/UserController.java
@@ -1,4 +1,53 @@
 package com.br.CostManagement.controller;
 
+import com.br.CostManagement.dto.request.user.UserAddSalaryDTO;
+import com.br.CostManagement.dto.request.user.UserCreateDTO;
+import com.br.CostManagement.dto.response.MessageDTO;
+import com.br.CostManagement.dto.response.user.UserDetailsDTO;
+import com.br.CostManagement.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/v1/cost/user")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<MessageDTO> create(@RequestBody @Validated UserCreateDTO userCreateDTO){
+        return ResponseEntity.ok(new MessageDTO(userService.create(userCreateDTO)));
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity<UserDetailsDTO> addSalary(@RequestBody @Validated UserAddSalaryDTO userAddSalaryDTO){
+        return ResponseEntity.ok(userService.addSalary(userAddSalaryDTO));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDetailsDTO>> findAll(){
+        return ResponseEntity.ok(userService.findAll());
+    }
+
+    @GetMapping(path = "/id")
+    public ResponseEntity<UserDetailsDTO> findById(@RequestParam(name = "id") Long id){
+        return ResponseEntity.ok(userService.findById(id));
+    }
+
+    @DeleteMapping
+    @Transactional
+    public ResponseEntity<Void> deleteById(@RequestParam(name = "id") Long id){
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
