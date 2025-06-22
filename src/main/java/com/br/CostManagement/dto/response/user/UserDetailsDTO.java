224a24f2cf4570e2bc88e1735dc50293067458e4
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-22 11:31:14 -0300

    create UserDetailsDTO

--- src/main/java/com/br/CostManagement/dto/response/user/UserDetailsDTO.java
+++ src/main/java/com/br/CostManagement/dto/response/user/UserDetailsDTO.java
@@ -1,4 +1,12 @@
 package com.br.CostManagement.dto.response.user;
 
-public record UserDetailsDTO() {
+import com.br.CostManagement.entity.User;
+
+import java.math.BigDecimal;
+
+public record UserDetailsDTO(Long id, String name, BigDecimal salary) {
+
+    public UserDetailsDTO(User user){
+        this(user.getId(), user.getName(), user.getSalary());
+    }
 }
