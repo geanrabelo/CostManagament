796306655be083a614d9b4d6faf0db13020f0bdc
geanrabelo <geancardosorabelomota@gmail.com>
2025-06-22 11:35:52 -0300

    create UserService

--- src/main/java/com/br/CostManagement/service/UserService.java
+++ src/main/java/com/br/CostManagement/service/UserService.java
@@ -1,4 +1,20 @@
 package com.br.CostManagement.service;
 
-public interface CategoryService {
+import com.br.CostManagement.dto.request.user.UserAddSalaryDTO;
+import com.br.CostManagement.dto.request.user.UserCreateDTO;
+import com.br.CostManagement.dto.response.user.UserDetailsDTO;
+
+import java.util.List;
+
+public interface UserService {
+
+    String create(UserCreateDTO userCreateDTO);
+
+    UserDetailsDTO addSalary(UserAddSalaryDTO userAddSalaryDTO);
+
+    UserDetailsDTO findById(Long id);
+
+    List<UserDetailsDTO> findAll();
+
+    void delete(Long id);
 }
